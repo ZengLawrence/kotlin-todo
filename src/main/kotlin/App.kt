49c5e5783855dae217f993d63f35bec425b8c14d
Lawrence Zeng <lawrence.zeng@egmail.com>
2023-05-13 15:21:06 -0400

    extract App class for functional tests

---
+++ src/main/kotlin/App.kt
@@ -0,0 +1,37 @@
+import controller.Controller
+import io.javalin.Javalin
+import io.javalin.apibuilder.ApiBuilder
+import io.javalin.http.HttpStatus
+import io.javalin.openapi.plugin.OpenApiPlugin
+import io.javalin.openapi.plugin.OpenApiPluginConfiguration
+import io.javalin.openapi.plugin.swagger.SwaggerConfiguration
+import io.javalin.openapi.plugin.swagger.SwaggerPlugin
+
+class App {
+
+    val instance = Javalin.create { config ->
+        config.plugins.register(OpenApiPlugin(OpenApiPluginConfiguration()
+            .withDefinitionConfiguration { _, definition ->
+                definition.withOpenApiInfo { openApiInfo ->
+                    openApiInfo.title = "Todo REST Service"
+                    openApiInfo.version = "1.0.0"
+                }
+            }
+        ))
+        config.plugins.register(SwaggerPlugin(SwaggerConfiguration()))
+    }.apply {
+        exception(Exception::class.java) { e, ctx -> e.printStackTrace() }
+        error(HttpStatus.NOT_FOUND) { ctx -> ctx.json("not found") }
+    }.routes {
+        ApiBuilder.path("todos") {
+            ApiBuilder.get(Controller::getAll)
+            ApiBuilder.post(Controller::create)
+            ApiBuilder.path("{id}") {
+                ApiBuilder.get(Controller::get)
+                ApiBuilder.put(Controller::update)
+                ApiBuilder.delete(Controller::delete)
+            }
+        }
+    }
+
+}
--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,36 +1,3 @@
-import controller.Controller
-import io.javalin.Javalin
-import io.javalin.apibuilder.ApiBuilder.*
-import io.javalin.http.HttpStatus
-import io.javalin.openapi.plugin.OpenApiPlugin
-import io.javalin.openapi.plugin.OpenApiPluginConfiguration
-import io.javalin.openapi.plugin.swagger.SwaggerConfiguration
-import io.javalin.openapi.plugin.swagger.SwaggerPlugin
-
 fun main() {
-    Javalin.create { config ->
-        config.plugins.register(OpenApiPlugin(OpenApiPluginConfiguration()
-            .withDefinitionConfiguration { _, definition ->
-                definition.withOpenApiInfo { openApiInfo ->
-                    openApiInfo.title = "Todo REST Service"
-                    openApiInfo.version = "1.0.0"
-                }
-            }
-        ))
-        config.plugins.register(SwaggerPlugin(SwaggerConfiguration()))
-    }.apply {
-        exception(Exception::class.java) { e, ctx -> e.printStackTrace() }
-        error(HttpStatus.NOT_FOUND) { ctx -> ctx.json("not found") }
-    }.routes {
-        path("todos") {
-            get(Controller::getAll)
-            post(Controller::create)
-            path("{id}") {
-                get(Controller::get)
-                put(Controller::update)
-                delete(Controller::delete)
-            }
-        }
-    }.start(7070)
-
+    App().instance.start(7070)
 }
