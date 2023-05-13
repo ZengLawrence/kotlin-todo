94ea4f48ba5cd17f444b239eb81493cc7730e540
Lawrence Zeng <lawrence.zeng@egmail.com>
2023-05-13 11:23:55 -0400

    add openapi annotations

--- src/main/kotlin/controller/Controller.kt
+++ src/main/kotlin/controller/Controller.kt
@@ -2,6 +2,8 @@ package controller
 
 import io.javalin.http.Context
 import io.javalin.http.bodyAsClass
+import io.javalin.openapi.*
+import org.eclipse.jetty.http.HttpStatus
 
 data class TodoDto(val id: Int, val description: String, val done: Boolean = false)
 data class NewTodoDto(val description: String)
@@ -13,14 +15,39 @@ val todos = mutableListOf<TodoDto>(
 )
 object Controller {
 
+    @OpenApi(
+        summary = "Get all todos",
+        tags = ["Read-only"],
+        responses = [OpenApiResponse("200", [OpenApiContent(Array<TodoDto>::class)])],
+        path = "/todos",
+        methods = [HttpMethod.GET]
+    )
     fun getAll(ctx: Context) {
         ctx.json(todos)
     }
 
+    @OpenApi(
+        summary = "Get a todo",
+        tags = ["Read-only"],
+        responses = [OpenApiResponse("200", [OpenApiContent(TodoDto::class)]),
+                    OpenApiResponse("404")],
+        path = "/todos/{id}",
+        pathParams = [OpenApiParam("id", Int::class)],
+        methods = [HttpMethod.GET]
+    )
     fun get(ctx: Context) {
-        ctx.json(todos.filter { it.id == ctx.pathParam("id").toInt() })
+        todos.find { it.id == ctx.pathParam("id").toInt() }
+            ?.also { ctx.json(it) } ?: ctx.status(HttpStatus.NOT_FOUND_404)
     }
 
+    @OpenApi(
+        summary = "create a new todo",
+        tags = ["Mutation"],
+        requestBody = OpenApiRequestBody([OpenApiContent(NewTodoDto::class)], required = true),
+        responses = [OpenApiResponse("201")],
+        path = "/todos",
+        methods = [HttpMethod.POST]
+    )
     fun create(ctx: Context) {
         val request = ctx.bodyAsClass<NewTodoDto>()
         val nextId = todos.maxOfOrNull(TodoDto::id)?.plus(1) ?: 1
@@ -29,6 +56,15 @@ object Controller {
         ctx.status(201)
     }
 
+    @OpenApi(
+        summary = "Update a todo",
+        tags = ["Mutation"],
+        requestBody = OpenApiRequestBody([OpenApiContent(TodoDto::class)], required = true),
+        responses = [OpenApiResponse("204")],
+        path = "/todos/{id}",
+        pathParams = [OpenApiParam("id", Int::class)],
+        methods = [HttpMethod.PUT]
+    )
     fun update(ctx: Context) {
         val todoDto = ctx.bodyAsClass<TodoDto>()
         todos.find { it.id == ctx.pathParam("id").toInt() }
@@ -36,9 +72,17 @@ object Controller {
                 todos.remove(it)
                 todos.add(todoDto)
             }
-        ctx.status(204)
+        ctx.status(HttpStatus.NO_CONTENT_204)
     }
 
+    @OpenApi(
+        summary = "Delete a todo",
+        tags = ["Mutation"],
+        responses = [OpenApiResponse("204")],
+        path = "/todos/{id}",
+        pathParams = [OpenApiParam("id", Int::class)],
+        methods = [HttpMethod.DELETE]
+    )
     fun delete(ctx: Context) {
             todos.removeIf { it.id == ctx.pathParam("id").toInt() }
             ctx.status(204)
