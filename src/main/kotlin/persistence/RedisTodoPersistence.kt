cab87a35a9eaf3d48a4ac5930f86dba2d90d30ef
Lawrence Zeng <lawrence.zeng@gmail.com>
2023-05-20 13:38:27 -0400

    Redis (#6)
    Implement persistence with Redis

---
+++ src/main/kotlin/persistence/RedisTodoPersistence.kt
@@ -0,0 +1,50 @@
+package persistence
+
+import redis.clients.jedis.UnifiedJedis
+import todo.PTodo
+import todo.TodoPersistence
+
+/**
+ * Redis based persistence implementation. This implementation is used for local develop only and
+ * not intend to be efficient.
+ */
+class RedisTodoPersistence(private val jedis: UnifiedJedis): TodoPersistence {
+    override fun insert(description: String, done: Boolean): Int {
+        val id = jedis.incr("todo-id").toInt()
+        val value = mapOf<String, String>(
+            "id" to "$id",
+            "description" to description,
+            "done" to "$done"
+        )
+        jedis.hset(key(id), value)
+        jedis.rpush("todo-ids", "$id")
+        return id
+    }
+
+    override fun update(id: Int, done: Boolean) {
+        jedis.hset(key(id), "done", "$done")
+    }
+
+    override fun delete(id: Int) {
+        jedis.lrem("todo-ids", 1, "$id")
+        jedis.del(key(id))
+    }
+
+    override fun find(id: Int): PTodo? {
+        val map = jedis.hgetAll(key(id))
+        return if (map.containsKey("id")) {
+            assert(map["id"]?.toInt() == id)
+            PTodo(id, map["description"] ?: "", map["done"].toBoolean())
+        } else {
+            null
+        }
+    }
+
+    override fun findAll(): List<PTodo> {
+        return jedis.lrange("todo-ids", 0, -1).map {
+            find(it.toInt())!!
+        }
+    }
+
+    private fun key(id: Int) = "todo:$id"
+}
---
+++ src/test/kotlin/persistence/RedisTodoPersistenceTest.kt
@@ -0,0 +1,85 @@
+package persistence
+
+import org.assertj.core.api.Assertions.*
+import org.junit.jupiter.api.BeforeEach
+import org.testcontainers.containers.GenericContainer
+import org.testcontainers.junit.jupiter.Container
+import org.testcontainers.junit.jupiter.Testcontainers
+import org.testcontainers.utility.DockerImageName
+import redis.clients.jedis.JedisPooled
+import todo.PTodo
+import kotlin.test.Test
+
+
+@Testcontainers
+class RedisTodoPersistenceTest {
+
+    @Container
+    val redis: GenericContainer<*> = GenericContainer(DockerImageName.parse("redis:alpine"))
+        .withExposedPorts(6379)
+
+    private lateinit var persistence: RedisTodoPersistence
+
+    @BeforeEach
+    @Throws(Exception::class)
+    fun setUp() {
+        val jedis = JedisPooled(redis.host, redis.getMappedPort(6379))
+        persistence = RedisTodoPersistence(jedis)
+    }
+
+
+    @Test
+    fun `insert multiple times should get different id each time`() {
+        val id1 = persistence.insert("Buy milk", done = false)
+        val id2 = persistence.insert("Get mail", done = true)
+        assertThat(id1).isNotEqualTo(id2)
+    }
+
+    @Test
+    fun `find by id should return a todo object`() {
+        val id = persistence.insert("Buy milk", done = false)
+        val actual = persistence.find(id)
+        assertThat(actual).isEqualTo(PTodo(id, "Buy milk", done = false))
+    }
+
+    @Test
+    fun `find by id does not return an object`() {
+        assertThat(persistence.find(-1)).isNull()
+    }
+
+    @Test
+    fun `delete a todo should not return one`() {
+        val id = persistence.insert("Buy milk", done = false)
+        val actual = persistence.find(id)
+        assertThat(actual).isEqualTo(PTodo(id, "Buy milk", done = false))
+
+        persistence.delete(id)
+        assertThat(persistence.find(id)).isNull()
+    }
+
+    @Test
+    fun `update todo to done should return with done to true`() {
+        val id = persistence.insert("Buy milk", done = false)
+        val actual = persistence.find(id)
+        assertThat(actual).isEqualTo(PTodo(id, "Buy milk", done = false))
+
+        persistence.update(id, done = true)
+        assertThat(persistence.find(id)).isEqualTo(PTodo(id, "Buy milk", done = true))
+    }
+
+    @Test
+    fun `find all should return all todos`() {
+        val id1 = persistence.insert("Buy milk", done = false)
+        val id2 = persistence.insert("Get newspaper", done = true)
+        val id3 = persistence.insert("Eat lunch", done = false)
+
+        assertThat(persistence.findAll()).containsOnlyElementsOf(
+            listOf(
+                PTodo(id1, "Buy milk", done = false),
+                PTodo(id2, "Get newspaper", done = true),
+                PTodo(id3, "Eat lunch", done = false)
+            )
+        )
+    }
+
+}
