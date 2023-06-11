90fcec5dc8dd64ffcd3ca9c06a27550195edee2a
Lawrence Zeng <lawrence.zeng@egmail.com>
2023-06-11 10:23:23 -0400

    use env map to simplify code

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -2,10 +2,11 @@ import app.App.Companion.app
 
 fun main() {
 
+    val env = System.getenv()
     app {
         redis {
-            System.getenv("REDIS_HOST")?.let { host = it }
-            System.getenv("REDIS_PORT")?.toInt()?.let { port = it }
+            env["REDIS_HOST"]?.let { host = it }
+            env["REDIS_PORT"]?.toInt()?.let { port = it }
         }
     }.build()
         .start(7070)
